32f94e0468cab6e3bad19c01eb9aef64192ae05a
Artem Maksimikhin <uanov86@gmail.com>
2021-04-08 18:18:15 +0300

    Updates

---
+++ precache-manifest.76fd883c33818980e983733a1f605949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdb4da4ece432b5c391fb8fcfbcf03b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "93aa583d71e70ecbd160",
+    "url": "./static/css/main.64ca048a.chunk.css"
+  },
+  {
+    "revision": "7d882953decced4f256c",
+    "url": "./static/js/2.1e2e7893.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1e2e7893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93aa583d71e70ecbd160",
+    "url": "./static/js/main.05b75a3f.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
